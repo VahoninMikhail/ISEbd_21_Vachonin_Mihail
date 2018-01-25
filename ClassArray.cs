889ad79e4ab2249bf45f814774edd8a61864e2b9
VahoninMikhail <31822003+VahoninMikhail@users.noreply.github.com>
2018-01-25 12:23:05 +0300

    Add files via upload

--- ClassArray.cs
+++ ClassArray.cs
@@ -6,49 +6,46 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication4
 {
-	class ClassArray<T> 
+	class ClassArray<T> where T : ITechnique
 	{
-		private T[] places;
+		//Словарь хранилище
+		private Dictionary<int, T> places;
+		//Макс количество
+		private int maxCount;
+		//Значение по умолчанию
 		private T defaultValue;
 
-		public ClassArray(int sizes, T defVal)
+		public ClassArray(int size, T defVal)
 		{
 			defaultValue = defVal;
-			places = new T[sizes];
-			for(int i = 0; i < places.Length; i++)
-			{
-				places[i] = defaultValue;
-			}
-		}
-
-		public T getObject(int ind)
-		{
-			if(ind > -1 && ind < places.Length)
-			{
-				return places[ind];
-			}
-			return defaultValue;
+			places = new Dictionary<int, T>();
+			maxCount = size;
 		}
 
 		public static int operator +(ClassArray<T> p, T plane)
 		{
-			for (int i = 0; i < p.places.Length; i++)
+			if (p.places.Count == p.maxCount)
+			{
+				return -1;
+			}
+			for (int i = 0; i < p.places.Count; i++)
 			{
 				if (p.CheckFreePlace(i))
 				{
-					p.places[i] = plane;
+					p.places.Add(i, plane);
 					return i;
 				}
 			}
-			return -1;
+			p.places.Add(p.places.Count, plane);
+			return p.places.Count - 1;
 		}
 
 		public static T operator -(ClassArray<T> p, int index)
 		{
-			if (!p.CheckFreePlace(index))
+			if (p.places.ContainsKey(index))
 			{
 				T plane = p.places[index];
-				p.places[index] = p.defaultValue;
+				p.places.Remove(index);
 				return plane;
 			}
 			return p.defaultValue;
@@ -56,22 +53,23 @@ namespace WindowsFormsApplication4
 
 		private bool CheckFreePlace(int index)
 		{
-			if (index < 0 || index > places.Length)
+			return !places.ContainsKey(index);
+		}
+
+		// И самое главное - индексатор:
+
+		public T this[int ind]
+		{
+			get
 			{
-				return false;
+				if (places.ContainsKey(ind))
+				{
+					return places[ind];
+				}
+				return defaultValue;
 			}
-			if (places[index] == null)
-			{
-				return true;
-			}
-			if (places[index].Equals(defaultValue))
-			{
-				return true;
-			}
-			return false;
+
 		}
 
 	}
-
-
 }
